021c7ce0d5cd19f7a5cb4a3505f72c576194b273
Fournext <sebastianzc04@gmail.com>
2025-06-01 21:01:48 -0400

    temporal

--- aula-inteligente-back/apirestfull/settings.py
+++ aula-inteligente-back/apirestfull/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-m9$ia0f^o^_*w1%oi0dsaw#=nix2+tktmkkfmp7jv^dme)w)s1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DB_aula_parcial_api',
         'USER': 'postgres',
-        'PASSWORD': '6305913',
+        'PASSWORD': '071104',
         'HOST': 'localhost',
         'PORT': '5432'
     }
